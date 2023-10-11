06a4d25a9201dd6a33a40f8ffda3ef92df3214f2
Denisse Abigail Guipzot Garibay <143037306+denisseGG7@users.noreply.github.com>
2023-10-11 01:21:30 -0700

    Update Practica2_1_1.py

--- Practica2_oled/Practica2_1_1.py
+++ Practica2_oled/Practica2_1_1.py
@@ -1,13 +1,13 @@
-#librerías
+#librerías y módulos
 import machine
 import ssd1306
 
-#comunicación y configuración de pines
+#comunicación y configuración de pines en la pantalla oled
 i2c = machine.I2C(0, sda=machine.Pin(8), scl=machine.Pin(9))
 displayOled = ssd1306.SSD1306_I2C(128, 64, i2c)
 
-displayOled.fill(0)
-displayOled.show()
 #Despliegue en pantalla oled
-displayOled.text("Hola mundo", 0, 0)
+displayOled.fill(0) #propiedades para el despliegue
 displayOled.show()
+displayOled.text("Hola mundo", 0, 0) #escribir texto en la pantalla
+displayOled.show() #mostrar texto
